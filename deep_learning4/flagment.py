b0e6cb750d3350c4c99c7fdcb0d226aa389c4202
Shuetsu Ito <shuetsu512@gmail.com>
2017-04-22 11:18:22 +0900

    数値微分・勾配

--- deep_learning4/flagment.py
+++ deep_learning4/flagment.py
@@ -1,7 +1,8 @@
 import sys, os
 sys.path.append(os.pardir)
 import numpy as np
-from common import mean_squared_error, cross_entropy_error
+from common import mean_squared_error, cross_entropy_error, \
+  numerical_diff, numerical_gradient
 from dataset.mnist import load_mnist
 
 t = [0, 0, 1, 0, 0]
@@ -22,3 +23,26 @@ print("MNISTデータ------------")
 
 print(x_train.shape)
 print(t_train.shape)
+
+print("ミニバッチ------------")
+
+batch_mask = np.random.choice(x_train.shape[0], 10)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+
+print("数値微分-------------")
+
+def function_1(x):
+    return 0.01*x**2 + 0.1*x
+
+print(numerical_diff(function_1, 5))
+print(numerical_diff(function_1, 10))
+
+print("勾配---------------")
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
