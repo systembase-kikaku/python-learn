9206c2baab4cbccbcfe77265edbc84788f458b38
Shuetsu Ito <shuetsu512@gmail.com>
2017-04-29 15:50:30 +0900

    誤差逆伝播法による学習

--- common.py
+++ common.py
@@ -11,12 +11,14 @@ def img_show(img):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-def softmax(a):
-    c = np.max(a)
-    exp_a = np.exp(a - c)
-    sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
-    return y
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
+    x = x - np.max(x)
+    return np.exp(x) / np.sum(np.exp(x))
 
 def mean_squared_error(y, t):
 	return 0.5 * np.sum((y-t)**2)
